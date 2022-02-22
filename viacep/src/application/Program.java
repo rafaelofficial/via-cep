f5f623c2c1a242828d148f2a1b82e20ab3ecc4a0
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-02-22 15:14:30 -0300

    implementação método findAll

--- viacep/src/application/Program.java
+++ viacep/src/application/Program.java
@@ -1,12 +1,24 @@
 package application;
 
+import java.util.List;
+
 import com.gtbr.ViaCepClient;
 import com.gtbr.domain.Cep;
+
+import dao.CepDao;
+import dao.DaoFactory;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Cep cep = ViaCepClient.findCep("13382440");
-		System.out.println(cep.getCep());	
+		
+		CepDao cepDao = DaoFactory.criaCepDao();
+		
+		//Cep cep = ViaCepClient.findCep("13382440");
+		
+		List<Cep> list = cepDao.findAll();
+		for (Cep listaCep : list) {
+			System.out.println(listaCep);
+		}
 	}
 }
--- viacep/src/dao/impl/CepDaoJdbc.java
+++ viacep/src/dao/impl/CepDaoJdbc.java
@@ -1,11 +1,19 @@
 package dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.gtbr.domain.Cep;
 
 import dao.CepDao;
+import db.DB;
+import db.DbException;
 
 public class CepDaoJdbc implements CepDao {
 	
@@ -23,8 +31,45 @@ public class CepDaoJdbc implements CepDao {
 
 	@Override
 	public List<Cep> findAll() {
-		// TODO Auto-generated method stub
+		
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			// preparar para a consulta sql
+			statement = conn.prepareStatement("SELECT * FROM tb_cidade");
+			// executa a query
+			resultSet = statement.executeQuery();
+			
+			List<Cep> list = new ArrayList<>();
+			Map<Integer, Cep> map = new HashMap();
+			
+			// testa se veio algum resultado
+			while (resultSet.next()) {
+				Cep cepMap = map.get(resultSet.getInt("Id"));
+				if (cepMap == null) {
+					Cep cep = new Cep();
+					cep.setCep(resultSet.getString("Cep"));
+					cep.setLogradouro(resultSet.getString("Logradouro"));
+					cep.setComplemento(resultSet.getString("Complemento"));
+					cep.setBairro(resultSet.getString("Bairro"));
+					cep.setLocalidade(resultSet.getString("Localidade"));
+					cep.setUf(resultSet.getString("Uf"));
+					cep.setIbge(resultSet.getString("Ibge"));
+					cep.setGia(resultSet.getString("Gia"));
+					cep.setDdd(resultSet.getString("Ddd"));
+					cep.setSiafi(resultSet.getString("siafi"));
+					
+					list.add(cep);
+				}
+				return list;
+			}	
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(statement);
+			DB.closeResultSet(resultSet);
+		}	
 		return null;
 	}
-
 }
