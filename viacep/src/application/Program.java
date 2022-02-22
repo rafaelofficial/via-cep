8f454c40032231f4cc1d9d72e3f0e13abb7cebed
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-02-21 23:14:34 -0300

    consumindo a API - Via Cep

--- viacep/src/application/Program.java
+++ viacep/src/application/Program.java
@@ -2,12 +2,17 @@ package application;
 
 import java.util.Scanner;
 
+import com.gtbr.ViaCepClient;
+import com.gtbr.domain.Cep;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		Cep cep = ViaCepClient.findCep("13382440");
+		System.out.println(cep);
 		sc.close();		
 	}
 }
